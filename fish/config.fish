87f43a9503fce8e368233e46d7970fc15020006e
ateschan <addisonteschan@gmail.com>
2024-07-18 19:02:33 -0500

    Gentoo move

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+set fish_greeting
+
+set -gx EDITOR "nvim"
+set -gx VISUAL "nvim"
+
+
+alias pastel "/home/ateschan/.cargo/bin/pastel" 
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
